1c6c0b3bd49c6eb57c20880f72148b81f599faa6
Benno Richters <b.richters@gmail.com>
2020-04-15 22:09:27 +0200

    split input row into separate class

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:letamind/blocs/game/game_bloc.dart';
 import 'package:letamind/screens/game/utils/size_data.dart';
+import 'package:letamind/screens/game/widgets/input_row.dart';
 import 'package:letamind/screens/game/widgets/letter_box.dart';
-import 'package:letamind/screens/game/widgets/letter_input_box.dart';
 
 class GameScreen extends StatefulWidget {
   GameScreen({Key key, this.title}) : super(key: key);
@@ -14,48 +14,9 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
-  // A FocusNode cannot be initialized in the build method
-  // See https://stackoverflow.com/questions/56038010/
-  final _controllers = <TextEditingController>[];
-  final _focusNodes = <FocusNode>[];
-
-  static const _maxWordLength = 8;
-  static const _emptyBoxChar = '_';
-  static const _textSelection = TextSelection(baseOffset: 0, extentOffset: 1);
-  static String _fixText(String value) =>
-      (value == null || value.trim().isEmpty)
-          ? _emptyBoxChar
-          : value.substring(0, 1).toUpperCase();
-
   @override
   void initState() {
     super.initState();
-
-    for (var i = 0; i < _maxWordLength; i++) {
-      final controller = TextEditingController()..text = _emptyBoxChar;
-      final focusNode = FocusNode();
-
-      focusNode.addListener(() {
-        if (focusNode.hasFocus) {
-          controller.value = TextEditingValue(
-            text: controller.text,
-            selection: _textSelection,
-          );
-        }
-      });
-
-      controller.addListener(() {
-        final fixedText = _fixText(controller.text);
-        controller.value = controller.value.copyWith(
-          text: fixedText,
-          composing: TextRange.empty,
-        );
-      });
-
-      _controllers.add(controller);
-      _focusNodes.add(focusNode);
-    }
-
     BlocProvider.of<GameBloc>(context).add(const StartNewGame());
   }
 
@@ -87,43 +48,9 @@ class _GameScreenState extends State<GameScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      children: [
-                        Row(
-                          children: _letterInputBoxes(
-                            context,
-                            state.wordLength,
-                            sizeData,
-                          ),
-                        ),
-                        space,
-                        Row(
-                          children: [
-                            _SubmitButton(
-                              sizeData: sizeData,
-                              color: Colors.amber,
-                              icon: const Icon(Icons.cloud_upload),
-                              onTap: () {
-                                final guess = _guessedWord(state.wordLength);
-                                _clearBoxes();
-                                _focusNodes[0].requestFocus();
-                                _controllers[0].value = TextEditingValue(
-                                  text: _controllers[0].text,
-                                  selection: _textSelection,
-                                );
-                                BlocProvider.of<GameBloc>(context)
-                                    .add(SubmitGuess(guess: guess));
-                              },
-                            ),
-                            _SubmitButton(
-                              sizeData: sizeData,
-                              color: Colors.red,
-                              icon: const Icon(Icons.cancel),
-                              onTap: () {},
-                            ),
-                          ],
-                        ),
-                      ],
+                    InputRow(
+                      length: state.wordLength,
+                      sizeData: sizeData,
                     ),
                     state.moves.isEmpty
                         ? Text('nothing')
@@ -156,67 +83,6 @@ class _GameScreenState extends State<GameScreen> {
 
         return const Center(child: CircularProgressIndicator());
       }),
-    );
-  }
-
-  List<LetterInputBox> _letterInputBoxes(
-    BuildContext context,
-    int length,
-    SizeData sizeData,
-  ) {
-    final result = <LetterInputBox>[];
-    for (var i = 0; i < length; i++) {
-      result.add(LetterInputBox(
-        sizeData: sizeData,
-        autofocus: i == 0,
-        controller: _controllers[i],
-        focusNode: _focusNodes[i],
-      ));
-    }
-
-    return result;
-  }
-
-  _guessedWord(int length) {
-    final result = StringBuffer();
-    for (var i = 0; i < length; i++) {
-      result.write(_controllers[i].text);
-    }
-
-    return result.toString();
-  }
-
-  void _clearBoxes() => _controllers.forEach((c) => c.text = _emptyBoxChar);
-}
-
-class _SubmitButton extends StatelessWidget {
-  const _SubmitButton({
-    @required this.sizeData,
-    @required this.color,
-    @required this.icon,
-    @required this.onTap,
-  });
-  final SizeData sizeData;
-  final Color color;
-  final Icon icon;
-  final GestureTapCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(sizeData.padding),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          width: sizeData.size,
-          height: sizeData.size,
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-          ),
-          child: Center(child: icon),
-        ),
-      ),
     );
   }
 }
---
+++ lib/screens/game/widgets/input_row.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:letamind/blocs/game/game_bloc.dart';
+import 'package:letamind/screens/game/utils/size_data.dart';
+
+import 'letter_input_box.dart';
+
+class InputRow extends StatefulWidget {
+  const InputRow({@required this.length, @required this.sizeData});
+  final int length;
+  final SizeData sizeData;
+
+  @override
+  State<StatefulWidget> createState() => _InputRowState();
+}
+
+class _InputRowState extends State<InputRow> {
+  // A FocusNode cannot be initialized in the build method
+  // See https://stackoverflow.com/questions/56038010/
+  final _controllers = <TextEditingController>[];
+  final _focusNodes = <FocusNode>[];
+
+  static const _maxWordLength = 8;
+  static const _emptyBoxChar = '_';
+  static const _textSelection = TextSelection(baseOffset: 0, extentOffset: 1);
+  static String _fixText(String value) =>
+      (value == null || value.trim().isEmpty)
+          ? _emptyBoxChar
+          : value.substring(0, 1).toUpperCase();
+
+  static const space = SizedBox(width: 10);
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (var i = 0; i < _maxWordLength; i++) {
+      final controller = TextEditingController()..text = _emptyBoxChar;
+      final focusNode = FocusNode();
+
+      focusNode.addListener(() {
+        if (focusNode.hasFocus) {
+          controller.value = TextEditingValue(
+            text: controller.text,
+            selection: _textSelection,
+          );
+        }
+      });
+
+      controller.addListener(() {
+        final fixedText = _fixText(controller.text);
+        controller.value = controller.value.copyWith(
+          text: fixedText,
+          composing: TextRange.empty,
+        );
+      });
+
+      _controllers.add(controller);
+      _focusNodes.add(focusNode);
+    }
+  }
+
+  @override
+  void dispose() {
+    _focusNodes.forEach((fn) => fn.dispose());
+    _controllers.forEach((c) => c.dispose());
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Row(
+          children: List.generate(
+            widget.length,
+            (i) => LetterInputBox(
+              sizeData: widget.sizeData,
+              autofocus: i == 0,
+              controller: _controllers[i],
+              focusNode: _focusNodes[i],
+            ),
+          ),
+        ),
+        space,
+        Row(
+          children: [
+            _SubmitButton(
+              sizeData: widget.sizeData,
+              color: Colors.amber,
+              icon: const Icon(Icons.cloud_upload),
+              onTap: () {
+                final guess = _guessedWord(widget.length);
+                _clearBoxes();
+                _focusNodes[0].requestFocus();
+                _controllers[0].value = TextEditingValue(
+                  text: _controllers[0].text,
+                  selection: _textSelection,
+                );
+                BlocProvider.of<GameBloc>(context)
+                    .add(SubmitGuess(guess: guess));
+              },
+            ),
+            _SubmitButton(
+              sizeData: widget.sizeData,
+              color: Colors.red,
+              icon: const Icon(Icons.cancel),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _guessedWord(int length) {
+    final result = StringBuffer();
+    for (var i = 0; i < length; i++) {
+      result.write(_controllers[i].text);
+    }
+
+    return result.toString();
+  }
+
+  void _clearBoxes() => _controllers.forEach((c) => c.text = _emptyBoxChar);
+}
+
+class _SubmitButton extends StatelessWidget {
+  const _SubmitButton({
+    @required this.sizeData,
+    @required this.color,
+    @required this.icon,
+    @required this.onTap,
+  });
+  final SizeData sizeData;
+  final Color color;
+  final Icon icon;
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(sizeData.padding),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: sizeData.size,
+          height: sizeData.size,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+          ),
+          child: Center(child: icon),
+        ),
+      ),
+    );
+  }
+}
